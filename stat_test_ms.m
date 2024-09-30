1461285725256aa731c2eb3e9be0a7ad7c450e7b
camglick <142433720+camglick@users.noreply.github.com>
2024-09-30 10:11:47 -0700

    Added stats script

---
+++ stat_test_ms.m
@@ -0,0 +1,69 @@
+%WGGGG
+%
+%
+%
+% aim - ttest for sptms paper
+% author - saggar@stanford.edu
+% date - Sep 23, 2024
+
+%%
+cd ~/Dropbox/Articles_In_Preparation/active/sptms-fmri/data_for_anova/
+
+fnames = dir('*.csv');
+fnames = {fnames.name};
+
+data = struct();
+for f = 1:1:length(fnames)
+    tmp = readtable(fnames{f});
+
+    data.(strrep(fnames{f},'.csv','')) = tmp;
+    
+
+end
+
+%%
+[p,s,st]=anova1(data.SUDS.value, data.SUDS.variable)
+multcompare(st)
+
+[p,s,st]=anova1(data.stim_snr.value, data.stim_snr.group)
+multcompare(st)
+[p,s,st]=anova1(data.rest_fd.value, data.rest_fd.group)
+multcompare(st)
+[p,s,st]=anova1(data.stim_fd.value, data.stim_fd.group)
+multcompare(st)
+
+[p,s,st]=anova1(data.tsnr_comparison.tsnr, data.tsnr_comparison.task)
+multcompare(st)
+
+
+[p,s,st]=anova1(data.efc_comparison.efc, data.efc_comparison.task)
+multcompare(st)
+
+[p,s,st]=anova1(data.fber_comparison.fber, data.fber_comparison.task)
+multcompare(st)
+
+[p,s,st]=anova1(data.snr_comparison.fber, data.snr_comparison.task)
+
+[p,s,st]=anova1(data.snr_comparison.snr, data.snr_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.dvars_std_comparison.dvars_std, data.dvars_std_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.dvars_vstd_comparison.dvars_vstd, data.dvars_vstd_comparison.task), multcompare(st)
+
+[p,s,st]=anova1(data.dvars_vstd_comparison.dvars_vstd, data.dvars_vstd_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.gcor_comparison.gcor, data.gcor_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.tsnr_comparison.tsnr, data.tsnr_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.fd_mean_comparison.fd_mean, data.fd_mean_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.gsr_x_comparison.gsr_x, data.gsr_x_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.gsr_x_comparison.gsr_y, data.gsr_y_comparison.task)
+[p,s,st]=anova1(data.gsr_y_comparison.gsr_y, data.gsr_y_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.aor_comparison.aor, data.aor_comparison.task)
+multcompare(st)
+[p,s,st]=anova1(data.aqi_comparison.aqi, data.aqi_comparison.task)
+multcompare(st)
